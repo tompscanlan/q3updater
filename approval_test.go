df033b5a52a0b2173e2d5e7824d98c87fad947f3
Tom Scanlan <tscanlan@vmware.com>
2016-09-29 23:15:40 -0400

    fix go vet

--- approval_test.go
+++ approval_test.go
@@ -21,20 +21,20 @@ var approvalTests = []struct {
 func CompareApproval(a *Approval, b *Approval, t *testing.T) {
 
 	if a.Id != b.Id {
-		t.Errorf("expected %s, got %s", a.Id, b.Id)
+		t.Errorf("expected %d, got %d", a.Id, b.Id)
 	}
 
 	if a.TeamId != b.TeamId {
-		t.Errorf("expected %s, got %s", a.TeamId, b.TeamId)
+		t.Errorf("expected %d, got %d", a.TeamId, b.TeamId)
 	}
 
 	if a.Blob != b.Blob {
-		t.Errorf("expected %s, got %s", a.Blob, b.Blob)
+		t.Errorf("expected %d, got %d", a.Blob, b.Blob)
 	}
 	if a.Description != b.Description {
 		t.Errorf("expected %s, got %s", a.Description, b.Description)
 	}
 	if a.Approved != b.Approved {
-		t.Errorf("expected %s, got %s", a.Approved, b.Approved)
+		t.Errorf("expected %t, got %t", a.Approved, b.Approved)
 	}
 }
